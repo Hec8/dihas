41e65eaa1fa793d2ae733f93acd3fa560fc24a6e
Hector <akpohector4@gmail.com>
2025-04-11 10:22:58 +0100

    image

--- components/Header/page.jsx
+++ components/Header/page.jsx
@@ -61,7 +61,7 @@ export default function Header() {
                             ) : (
                                 <Link href="/">
                                     <Image
-                                        src="/assets/logo 1.png"
+                                        src="/assets/logo1.png"
                                         alt="Diha's tech"
                                         width={100}
                                         height={40}
