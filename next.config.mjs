cbc14acc4609cb0333032e52d035d8eb6aed5b5f
Hector <akpohector4@gmail.com>
2025-05-09 10:11:02 +0100

    config22

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,20 @@ const nextConfig = {
       },
       // Ajoutez d'autres redirections si nécessaire
     ]
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://dihas.vercel.app' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig;
