e98c0c80b44ec91c650b9daa396aa3d58972759e
Hector <akpohector4@gmail.com>
2025-05-09 09:49:27 +0100

    config21

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,22 @@ const nextConfig = {
         port: '8000',
         pathname: '/images/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'negative-honor-hec8-2159b031.koyeb.app', // Retirez le 'https://' ici
+        pathname: '/images/**'
+      }
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://negative-honor-hec8-2159b031.koyeb.app/api/:path*',
+      },
+      // Ajoutez d'autres redirections si nécessaire
+    ]
+  }
 }
 
 export default nextConfig;
