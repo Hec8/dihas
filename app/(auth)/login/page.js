94c2b1e6e1f98f9ec09e2577cccbf6d03833f65d
Hector <akpohector4@gmail.com>
2025-05-12 19:48:00 +0100

    ajustement login 1

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -199,7 +199,7 @@ const LoginContent = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                         </svg>
-                        Retour à l'accueil
+                        Retour à l&apos;accueil
                     </Link>
                     
                     <div className="flex items-center">
